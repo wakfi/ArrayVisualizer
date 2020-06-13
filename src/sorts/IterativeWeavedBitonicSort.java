bc75abf3cd3ff85a68f95e22ff1d3b4a6d906baf
aphitorite <pocketxan2@gmail.com>
2020-06-12 21:25:52 -0600

    + Weaved Bitonic Sorts

---
+++ src/sorts/IterativeWeavedBitonicSort.java
@@ -0,0 +1,87 @@
+package sorts;
+
+import templates.Sort;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2020 aphitorite
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class IterativeWeavedBitonicSort extends Sort {
+    public IterativeWeavedBitonicSort(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("Iter. Weaved Bitonic");
+        this.setRunAllID("Iterative Weaved Bitonic Sort");
+        this.setReportSortID("Iterative Weaved Bitonic Sort");
+        this.setCategory("Concurrent Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+	
+	private void iterativeCircle(int[] array, int pos, int len, int trueLen, int divisions) {
+		int i = 0, d = (len/2)/divisions;
+		
+		boolean inBounds1, inBounds2;
+		for(int j = len-d; j >= len/2; j-=d) {
+			for(int k = 0; k < d; k++) {
+				inBounds1 = pos+i < trueLen;
+				inBounds2 = pos+j+k < trueLen;
+				
+				if(inBounds1)
+					Highlights.markArray(1, pos+i);
+				else
+					Highlights.clearMark(1);
+				
+				if(inBounds2)
+					Highlights.markArray(2, pos+j+k);
+				else
+					Highlights.clearMark(2);
+				
+				if(inBounds1) Delays.sleep(0.5);
+				if(inBounds2 && Reads.compare(array[pos+i], array[pos+j+k]) == 1)
+					Writes.swap(array, pos+i, pos+j+k, 0.5, true, false);
+				i++;
+			}
+		}
+	}
+
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+		int paddedLength = (int)Math.pow(2, Math.ceil(Math.log(length)/Math.log(2)));
+		
+		for(int i = 1; i < paddedLength; i*=2)
+			for(int j = 1; j <= i; j*=2)
+				for(int k = 0; k < paddedLength; k += paddedLength/j)
+					this.iterativeCircle(array, k, paddedLength/j, length, i/j);
+    }
+}
---
+++ src/sorts/RecursiveWeavedBitonicSort.java
@@ -0,0 +1,93 @@
+package sorts;
+
+import templates.Sort;
+import utils.Delays;
+import utils.Highlights;
+import utils.Reads;
+import utils.Writes;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2020 aphitorite
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class RecursiveWeavedBitonicSort extends Sort {
+    public RecursiveWeavedBitonicSort(Delays delayOps, Highlights markOps, Reads readOps, Writes writeOps) {
+        super(delayOps, markOps, readOps, writeOps);
+        
+        this.setSortPromptID("Rec. Weaved Bitonic");
+        this.setRunAllID("Recursive Weaved Bitonic Sort");
+        this.setReportSortID("Recursive Weaved Bitonic Sort");
+        this.setCategory("Concurrent Sorts");
+        this.isComparisonBased(true);
+        this.isBucketSort(false);
+        this.isRadixSort(false);
+        this.isUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.isBogoSort(false);
+    }
+	
+	private void circle(int[] array, int pos, int len, int trueLen, int gap) {
+		if(len < 2)
+			return;
+		
+		boolean inBounds1, inBounds2;
+		for(int i = 0; 2*i < (len-1)*gap; i+=gap) {
+			inBounds1 = pos+i < trueLen;
+			inBounds2 = pos+(len-1)*gap-i < trueLen;
+			
+			if(inBounds1)
+				Highlights.markArray(1, pos+i);
+			else
+				Highlights.clearMark(1);
+			
+			if(inBounds2)
+				Highlights.markArray(2, pos+(len-1)*gap-i);
+			else
+				Highlights.clearMark(2);
+			
+			if(inBounds1) Delays.sleep(0.5);
+			if(inBounds2 && Reads.compare(array[pos+i], array[pos+(len-1)*gap-i]) == 1)
+				Writes.swap(array, pos+i, pos+(len-1)*gap-i, 0.5, true, false);
+		}
+		
+		this.circle(array, pos, len/2, trueLen, gap);
+		this.circle(array, pos+len*gap/2, len/2, trueLen, gap);
+	}
+	
+	private void weaveCircle(int[] array, int pos, int len, int trueLen, int gap) {
+		if(len < 2)
+			return;
+		
+		this.weaveCircle(array, pos, len/2, trueLen, 2*gap);
+		this.weaveCircle(array, pos+gap, len/2, trueLen, 2*gap);
+		
+		this.circle(array, pos, len, trueLen, gap);
+	}
+
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+		this.weaveCircle(array, 0, (int)Math.pow(2, Math.ceil(Math.log(length)/Math.log(2))), length, 1);
+    }
+}
